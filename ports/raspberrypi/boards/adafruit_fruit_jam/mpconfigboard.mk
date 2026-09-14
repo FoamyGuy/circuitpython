5adf5fc00eff6e3cdc8d5dfe5f4de890b6f9b3f0
foamyguy <foamyguy@gmail.com>
2026-09-14 08:28:55 -0500

    usb host camera inital support

--- ports/raspberrypi/boards/adafruit_fruit_jam/mpconfigboard.mk
+++ ports/raspberrypi/boards/adafruit_fruit_jam/mpconfigboard.mk
@@ -14,5 +14,11 @@ CIRCUITPY_SDIOIO = 1
 CIRCUITPY_PICOGAME = 1
 CIRCUITPY_PICOGAME_FRAMEBUFFER = 1
 
+# USB video (UVC) cameras have configuration descriptors of several KB.
+# TinyUSB skips enumerating any device whose descriptor doesn't fit here.
+CFLAGS += -DCFG_TUH_ENUMERATION_BUFSIZE=4096
+# Buffer isochronous IN packets (such as UVC video) between reads.
+CFLAGS += -DPIO_USB_ISO_RING_SIZE=16384
+
 # CIRCUITPY_DISPLAY_FONT = $(TOP)/tools/fonts/unifont-16.0.02-all.bdf
 # CIRCUITPY_FONT_EXTRA_CHARACTERS = "🖮🖱️"
